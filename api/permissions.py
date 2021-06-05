5eae265d2bb48eff5631f76b636b7c3379d26f11
viktoria kolomiets <kolomietsvvv@gmail.com>
2021-06-05 14:48:18 +0300

    fixed syntax error

--- api/permissions.py
+++ api/permissions.py
@@ -5,8 +5,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return (
-                request.user.role == 'admin' or
-                request.method in permissions.SAFE_METHODS
+                request.user.role == 'admin'
+                or request.method in permissions.SAFE_METHODS
             )
         return request.method in permissions.SAFE_METHODS
 
@@ -15,9 +15,9 @@ class OwnerAdminModeratorReadonly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.user.is_authenticated:
             return (
-                request.user.role in ['moderator', 'admin'] or
-                request.method in permissions.SAFE_METHODS or
-                obj.author == request.user
+                request.user.role in ['moderator', 'admin']
+                or request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
             )
         return request.method in permissions.SAFE_METHODS
 
