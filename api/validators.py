bd8de373241d833ebb3a63f7b9e10295eedb74cf
ArturioNe <artur.negovora@yandex.ru>
2021-06-06 15:20:35 +0300

    after 1 review

--- api/validators.py
+++ api/validators.py
@@ -7,4 +7,8 @@ from rest_framework.serializers import ValidationError
 
 def validator_year(value):
     if -5500 > value > datetime.date.today().year:
-        raise ValidationError(_('Несуществующий год', params={'value': value}))
+        raise ValidationError(
+            _('Несуществующий год',
+              code='invalid',
+              params={'value': value})
+        )
