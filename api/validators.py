0da49dc404fa5d4cebbc3642e8203ae64f8ad663
ArturioNe <artur.negovora@yandex.ru>
2021-06-06 15:11:01 +0300

    after 1 review

--- api/validators.py
+++ api/validators.py
@@ -1,8 +1,10 @@
 import datetime
 
+from django.utils.translation import ugettext as _
+
 from rest_framework.serializers import ValidationError
 
 
 def validator_year(value):
     if -5500 > value > datetime.date.today().year:
-        raise ValidationError('Несуществующий год')
+        raise ValidationError(_('Несуществующий год', params={'value': value}))
