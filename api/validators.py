1241094591fabc071dd3182aa4e6903ffc80bcc8
Артур <78036105+ArturioNe@users.noreply.github.com>
2021-06-07 14:57:43 +0300

    Update validators.py
    исправил валидатор.

--- api/validators.py
+++ api/validators.py
@@ -1,12 +1,11 @@
 import datetime
-from django.utils.translation import ugettext as _
 from rest_framework.serializers import ValidationError
 
 
 def validator_year(value):
     if -5500 > value > datetime.date.today().year + 50:
         raise ValidationError(
-            _('Несуществующий год',
-              code='invalid',
-              params={'value': value})
+            'Несуществующий год',
+            code='invalid',
+            params={'value': value}
         )
