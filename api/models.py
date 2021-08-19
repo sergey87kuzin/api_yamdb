28fe43b092fc5e804f393d174b21f92ffe9f83a0
sergey87kuzin <sergeykuzin@sergeys-MacBook-Air.local>
2021-08-19 17:01:04 +0300

    checked admin

--- api/models.py
+++ api/models.py
@@ -6,6 +6,80 @@ from django.db import models
 from .validators import validator_year
 
 
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email), **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        return self.create_user(
+            email=email, role=settings.ADMIN, password=password,
+            **extra_fields
+        )
+
+    def all(self):
+        return self.get_queryset()
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    first_name = models.CharField(max_length=30, blank=True, null=True,
+                                  verbose_name='first_name')
+    last_name = models.CharField(max_length=30, blank=True,
+                                 verbose_name='last_name')
+    username = models.CharField(max_length=40, unique=True,
+                                verbose_name='username')
+    bio = models.TextField(blank=True, null=True, verbose_name='biography')
+    role = models.CharField(max_length=10, choices=settings.ROLES, blank=True,
+                            default=settings.ADMIN, verbose_name='role')
+    password = models.CharField(max_length=128, verbose_name='password',
+                                blank=True)
+    confirmation_code = models.CharField(max_length=30, blank=True,
+                                         verbose_name='token')
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.role == settings.ADMIN
+
+    @property
+    def is_admin(self):
+        return self.role == settings.ADMIN
+
+    @property
+    def is_not_user(self):
+        return self.role != settings.USER
+
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, **extra_fields):
@@ -20,44 +94,6 @@ class UserManager(BaseUserManager):
 
     def all(self):
         return self.get_queryset()
-
-
-class User(AbstractBaseUser):
-    email = models.EmailField(max_length=40, unique=True, verbose_name='email')
-    first_name = models.CharField(max_length=30, blank=True, null=True,
-                                  verbose_name='first_name')
-    last_name = models.CharField(max_length=30, blank=True,
-                                 verbose_name='last_name')
-    username = models.CharField(max_length=40, unique=True,
-                                verbose_name='username')
-    bio = models.TextField(blank=True, null=True, verbose_name='biography')
-    role = models.CharField(max_length=10, choices=settings.ROLES, blank=True,
-                            default=settings.USER, verbose_name='role')
-    password = models.CharField(max_length=128, verbose_name='password',
-                                blank=True)
-    confirmation_code = models.CharField(max_length=30, blank=True,
-                                         verbose_name='token')
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', ]
-
-    class Meta:
-        verbose_name = 'user'
-        verbose_name_plural = 'users'
-        ordering = ('-id',)
-
-    def __str__(self):
-        return self.email
-
-    @property
-    def is_admin(self):
-        return self.role == settings.ADMIN
-
-    @property
-    def is_not_user(self):
-        return self.role != settings.USER
 
 
 class Title(models.Model):
